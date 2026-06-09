522ec90258e56a8a27bfef66b0965e71526e6f69
gliadev <gliatem@gmail.com>
2026-06-10 00:16:40 +0200

    Initial Commit

---
+++ MedicAppRemind/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MedicAppRemind
+//
+//  Created by Adolfo on 10/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
